e27b6ed31914e32fe4ada008b1b44e1b3cf59b22
0l9h <olehbobeliak@gmail.com>
2020-04-29 02:32:10 +0300

    You can now change another file

--- replace.py
+++ replace.py
@@ -1,3 +1,6 @@
+from colorama import init
+from colorama import Fore, Back, Style
+init()
 
 class Replacement():
     i = 0
@@ -19,19 +22,48 @@ class Replacement():
             else:
                 self.new_string += self.old_string[Replacement.i]          #   else: add char old_string[i] to new_string
             Replacement.i+=1
+        print(Replacement.i)
         print(self.new_string)
+        print(Fore.GREEN+Style.BRIGHT+'Succesful')
 
+    """change another file"""
+    def OutsideReplace(self, file_name):
+        self.new_text = ''
+        self.file_name = file_name
+        with open(self.file_name, 'r') as read_file:
+            reading = read_file.read()
+
+        while Replacement.i < len(reading):
+            #   if any interval in reading == what_to_replace
+            if reading[(Replacement.i) : (Replacement.i+len(self.what_to_replace))] == self.what_to_replace: 
+                self.new_text += self.replace_for                             #   change it to replace_for
+                Replacement.i += (len(self.what_to_replace)-1)             #   skip next values
+            else:
+                self.new_text += reading[Replacement.i]          #   else: add char old_string[i] to new_string
+            Replacement.i+=1
+        
+        with open(self.file_name, 'w') as write_to_file:
+            write_to_file.write(self.new_text)
+
+        print(Fore.GREEN+Style.BRIGHT+'Succesful')
 
 
 choice = input('Print "input" if you want to change string inside program.\n\
 Print "outside" if you want to change another file:\n\
 Print "exit" to stop executing:\n')
 
-if choice != 'exit':
-    what_to_replace = input('Input chars you need to replace:\n')
-    replace_for = input('Input replacement chars:\n')
-    getClass = Replacement(what_to_replace, replace_for)
 
 if choice == 'input':
     old_string = input('Input string:\n')
+    what_to_replace = input('Input chars you need to replace:\n')
+    replace_for = input('Input replacement chars:\n')
+    getClass = Replacement(what_to_replace, replace_for)
     getClass.InputReplace(old_string)
+
+elif choice == 'outside':
+    file_name = input('Print name of file you want to change:\n')
+    what_to_replace = input('Input chars you need to replace:\n')
+    replace_for = input('Input replacement chars:\n')
+    getClass = Replacement(what_to_replace, replace_for)
+    getClass.OutsideReplace(file_name)
+
